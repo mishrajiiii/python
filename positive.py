e3267547f410fdda5608cd04f75e2198549cf048
mishrajiiii <93723618+mishrajiiii@users.noreply.github.com>
2022-02-16 17:29:27 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,14 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+print("for list 1")
+for num in list1:
+    if num >= 0:
+       print(num, end = " ")
+       
+print("\n")
+       
+print("for list 2")       
+for num in list2:
+    if num >= 0:
+       print(num, end = " ")  
